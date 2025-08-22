a48ba84560c370e0d9c5bc993679f8f03d03f342
Daniyal Ahmed <da9033431@gmail.com>
2025-08-22 14:35:21 +0100

    eslint and axios setup

---
+++ src/config/axiosInstance.js
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
+
+const instance = axios.create({
+  baseURL: BACKEND_URL,
+});
+
+export default instance;
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+import { dirname, resolve } from "path";
+import { fileURLToPath } from "url";
+import { defineConfig } from "vite";
 
-// https://vite.dev/config/
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 export default defineConfig({
-  plugins: [ tailwindcss(),react()],
-})
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      Components: resolve(__dirname, "src/components"),
+      Pages: resolve(__dirname, "src/pages"),
+      Configs: resolve(__dirname, "src/configs"),
+      Utils: resolve(__dirname, "src/utils"),
+      Store: resolve(__dirname, "src/store"),
+      Assets: resolve(__dirname, "src/assets"),
+      Routes: resolve(__dirname, "src/routes"),
+      Apis: resolve(__dirname, "src/apis"),
+      Hooks: resolve(__dirname, "src/hooks"),
+    },
+  },
+});
