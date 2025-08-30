ec37c09afea17db43ee7702af87b5664025bc111
Daniyal Ahmed <da9033431@gmail.com>
2025-08-30 15:45:58 +0100

    refactoring

--- src/apis/project.js
+++ src/apis/project.js
@@ -3,7 +3,6 @@ import instance from "Configs/axiosInstance";
 export const createProjectApi = async () => {
   try {
     const response = await instance.post("/api/v1/projects");
-    console.log(response.data);
     return response.data;
   } catch (error) {
     console.log(error);
