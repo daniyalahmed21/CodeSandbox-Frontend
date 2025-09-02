5d2be9d951cfbb037d232e7c5acaec0b81d3fe65
Daniyal Ahmed <da9033431@gmail.com>
2025-09-02 17:45:24 +0100

    refactoring

--- src/components/molecules/EditorComponent.jsx
+++ src/components/molecules/EditorComponent.jsx
@@ -3,9 +3,13 @@ import Editor, { useMonaco } from "@monaco-editor/react";
 import ThemeSelector from "Components/atoms/ThemeSelector";
 import { useActiveFileTabStore } from "Store/ActiveFileTabStore";
 import { useEditorSocketStore } from "Store/EditorSocketStore";
+import { getLanguageFromExtension } from "Utils/EditorUtils";
 
 import { customThemes } from "../../../themes/index";
+
+
 let timerId;
+
 const EditorComponent = () => {
   const monaco = useMonaco();
   const [theme, setTheme] = useState("vs-dark");
@@ -14,38 +18,32 @@ const EditorComponent = () => {
 
   useEffect(() => {
     if (!monaco) return;
-
     Object.entries(customThemes).forEach(([name, data]) => {
       monaco.editor.defineTheme(name, data);
     });
-
     monaco.editor.setTheme(theme);
   }, [monaco, theme]);
 
   const handleOnChange = (value) => {
-    console.log("Emitting writeFile:", { value, path: activeFileTab?.path })
-    if(timerId) clearTimeout(timerId);
-    timerId = setTimeout(()=>{
+    if (!activeFileTab?.path) return;
+
+    if (timerId) clearTimeout(timerId);
+    timerId = setTimeout(() => {
       socketEditor.emit("writeFile", {
         data: value,
         pathOfFileOrFolder: activeFileTab.path,
       });
-    },2000)
-    
-
-    
+    }, 2000);
   };
+
   return (
     <div className="w-full h-screen flex flex-col bg-slate-800">
       <ThemeSelector onThemeChange={setTheme} currentTheme={theme} />
       <div className="flex-grow">
         <Editor
           height="100%"
-          value={
-            activeFileTab?.value
-              ? activeFileTab?.value
-              : "// Your code goes here"
-          }
+          value={activeFileTab?.value ?? "// Your code goes here"}
+          language={getLanguageFromExtension(activeFileTab?.extension)}
           theme={theme}
           onChange={handleOnChange}
           options={{
